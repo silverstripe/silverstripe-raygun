82d7be8698480ede396faff74298f8e3adb091a3
Sam Minnee <sam@silverstripe.com>
2013-11-20 12:33:03 +1300

    NEW: Throw warning if you haven't configured the module properly on a live server.

--- _config.php
+++ _config.php
@@ -7,4 +7,8 @@ if(defined('SS_RAYGUN_APP_KEY')) {
 	// Disabled for now pending changes to SS Framework to allow for exception-handling override
 	//$originalExceptionHandler = set_exception_handler(array($raygun, 'exception_handler'));
 	//$raygun->setPostExceptionCallback($originalExceptionHandler);
+} else {
+	if(Director::isLive()) {
+		user_error("SilverStripe RayGun module installed, but SS_RAYGUN_APP_KEY not defined in _ss_environment.php", E_USER_WARNING);
+	}
 }
