adfa30bad7ca9ebe07cdd65a008217d10af5d3f3
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2021-10-20 12:11:59 +1300

    ENH Allow configuration of what user data is sent.

--- src/RaygunHandler.php
+++ src/RaygunHandler.php
@@ -5,10 +5,21 @@ namespace SilverStripe\Raygun;
 use Raygun4php\RaygunClient;
 use SilverStripe\Core\Config\Config;
 use Graze\Monolog\Handler\RaygunHandler as MonologRaygunHandler;
+use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Security\Security;
 
 class RaygunHandler extends MonologRaygunHandler
 {
+    use Configurable;
+
+    private static $user_main_id_field = 'Email';
+
+    private static $user_include_firstname = false;
+
+    private static $user_include_fullname = false;
+
+    private static $user_include_email = false;
+
     protected function write(array $record)
     {
         $disableTracking = Config::inst()->get(
@@ -20,7 +31,13 @@ class RaygunHandler extends MonologRaygunHandler
         if (!$disableTracking) {
             $user = Security::getCurrentUser();
             if ($user) {
-                $this->client->SetUser($user->Email);
+                $idField = $this->config()->get('user_main_id_field');
+                $this->client->SetUser(
+                    (string)$user->$idField,
+                    (bool)$this->config()->get('user_include_firstname') ? $user->FirstName : null,
+                    (bool)$this->config()->get('user_include_fullname') ? $user->getName() : null,
+                    (bool)$this->config()->get('user_include_email') ? $user->Email : null
+                );
             }
         }
 
