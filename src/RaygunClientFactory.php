c2d96ba58c764153a2871721b97d8330fed79130
Chris Penny <chris.penny@gmail.com>
2023-02-23 11:30:41 +1300

    Remove support for client v1

--- src/RaygunClientFactory.php
+++ src/RaygunClientFactory.php
@@ -3,19 +3,15 @@
 namespace SilverStripe\Raygun;
 
 use GuzzleHttp\Client;
-use LogicException;
 use Psr\SimpleCache\CacheInterface;
 use Raygun4php\RaygunClient;
 use Raygun4php\Transports\GuzzleAsync;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Config\Config;
-use SilverStripe\Core\CoreKernel;
 use SilverStripe\Core\Environment;
 use SilverStripe\Core\Flushable;
 use SilverStripe\Core\Injector\Factory;
 use SilverStripe\Core\Injector\Injector;
-use SilverStripe\Core\Kernel;
-use SilverStripe\Core\Path;
 
 class RaygunClientFactory implements Factory, Flushable
 {
@@ -57,42 +53,6 @@ class RaygunClientFactory implements Factory, Flushable
         );
         $disableTracking = is_bool($disableTracking) ? $disableTracking : false;
 
-        // Setup new client in the way that is best for the current SDK version.
-        if (substr(ltrim(static::getSdkVersion(), 'v'), 0, 2) === '1.') {
-            $this->createForV1($apiKey, $disableTracking, $params);
-        } else {
-            $this->createForV2($apiKey, $disableTracking, $params);
-        }
-
-        $this->filterSensitiveData();
-
-        return $this->client;
-    }
-
-    protected function createForV1($apiKey, $disableTracking, $params)
-    {
-        // Instantiate actual client
-        $this->client = new RaygunClient(
-            $apiKey,
-            true,
-            false,
-            $disableTracking
-        );
-
-        // Set proxy
-        if (!empty($params['proxyHost'])) {
-            $proxy = $params['proxyHost'];
-
-            if (!empty($params['proxyPort'])) {
-                $proxy .= ':' . $params['proxyPort'];
-            }
-
-            $this->client->setProxy($proxy);
-        }
-    }
-
-    protected function createForV2($apiKey, $disableTracking, $params)
-    {
         // Prepare transport config.
         $transportConfig = [
             'base_uri' => 'https://api.raygun.com',
@@ -121,6 +81,10 @@ class RaygunClientFactory implements Factory, Flushable
 
         // Ensure asynchronous requests are given time to finish.
         register_shutdown_function([$transport, 'wait']);
+
+        $this->filterSensitiveData();
+
+        return $this->client;
     }
 
     protected function filterSensitiveData()
@@ -140,46 +104,6 @@ class RaygunClientFactory implements Factory, Flushable
             'Authorization' => true,
             'Cookie' => true,
         ]);
-    }
-
-    /**
-     * Get the currently installed version of the raygun4php package according to composer.lock
-     *
-     * @return string
-     */
-    public static function getSdkVersion()
-    {
-        $cache = Injector::inst()->get(CacheInterface::class . '.raygunCache');
-        /** @var CoreKernel $kernel */
-        $kernel = Injector::inst()->get(Kernel::class);
-
-        // If the SDK version isn't cached, get it from the composer.lock file.
-        // Note that this is called before flushing has occurred - if we're flushing, bypass the cache for now.
-        if ($kernel->isFlushed() || !$version = $cache->get('raygun4phpVersion')) {
-            $composerLockRaw = file_get_contents(Path::join(Director::baseFolder(), 'composer.lock'));
-
-            if (!$composerLockRaw) {
-                throw new LogicException('composer.lock file is missing.');
-            }
-
-            $packageList = json_decode($composerLockRaw, true)['packages'];
-
-            foreach ($packageList as $package) {
-                if ($package['name'] === 'mindscape/raygun4php') {
-                    $version = $package['version'];
-                    break;
-                }
-            }
-
-            if (!$version) {
-                throw new LogicException('mindscape/raygun4php not found in composer.lock');
-            }
-
-            // Cache the SDK version so we don't have to do this every request.
-            $cache->set('raygun4phpVersion', $version);
-        }
-
-        return $version;
     }
 
     public static function flush()
